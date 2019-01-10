c7c0a9d6d3d42b6753b02c565b673a0294b9bfa7
Jane Lee <Yesdoing13@gmail.com>
2019-01-10 15:16:01 +0900

    Styling Log In Screen

--- screens/LogInScreen/presenter.js
+++ screens/LogInScreen/presenter.js
@@ -9,74 +9,138 @@ import {
   TextInput,
   StatusBar
 } from "react-native";
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons } from "@expo/vector-icons";
 
 const { width, height } = Dimensions.get("window");
 
 const LoginScreen = props => (
-    <View style={styles.container}>
-        <StatusBar barStyle={"light-content"}/>
-        <View style={styles.header}>
-            <Image 
-                source={require("../../assets/images/logo-white.png")}
-                resizeMode="stretch"
-                style={styles.logo}
-            />
-        </View>
-        <View style={styles.content}>
-            <TextInput placeholder="Username" />
-            <TextInput placeholder="Password" />
-            <TouchableOpacity>
-                <View>
-                    <Text>Log In</Text>
-                </View>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.fbContainer}>
-                <View style={styles.fbView}>
-                    <Ionicons name="logo-facebook" size={22} color="#3E99EE"/>
-                    <Text style={styles.fbText}>Log in with Facebook</Text>
-                </View>
-            </TouchableOpacity>
-        </View>
+  <View style={styles.container}>
+    <StatusBar barStyle={"light-content"} />
+    <View style={styles.header}>
+      <Image
+        source={require("../../assets/images/logo-white.png")}
+        resizeMode="stretch"
+        style={styles.logo}
+      />
     </View>
+    <View style={styles.content}>
+      <TextInput
+        placeholder="Username"
+        style={styles.textInput}
+        autoCapitalize={"none"}
+        autoCorrect={false}
+      />
+      <TextInput
+        placeholder="Password"
+        style={styles.textInput}
+        autoCapitalize={"none"}
+        secureTextEntry={true}
+      />
+      <TouchableOpacity style={styles.touchable}>
+        <View style={styles.button}>
+          <Text style={styles.btnText}>Log In</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.fbContainer}>
+        <View style={styles.fbView}>
+          <Ionicons name="logo-facebook" size={22} color="#3E99EE" />
+          <Text style={styles.fbText}>Log in with Facebook</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+    <View style={styles.signupView}>
+        <Text style={styles.signupText}>
+            Don't have an account?
+        </Text>
+        <Text style={styles.signupLink}>
+            Sign up.
+        </Text>
+    </View>
+  </View>
 );
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
+  container: {
+    flex: 1
+  },
+  header: {
+    flex: 1,
+    backgroundColor: "#4E65B4",
+    alignItems: "center",
+    justifyContent: "center",
+    width
+  },
+  logo: {
+    width: 180,
+    height: 65,
+    marginTop: 20
+  },
+  content: {
+    flex: 4,
+    backgroundColor: "white",
+    paddingTop: 50,
+    alignItems: "center",
+    justifyContent: "flex-start"
+  },
+  fbContainer: {
+    marginTop: 50
+  },
+  fbView: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  fbText: {
+    color: "#3E99EE",
+    marginLeft: 10,
+    fontWeight: "600",
+    fontSize: 14
+  },
+  textInput: {
+    height: 50,
+    borderColor: "#bbb",
+    borderWidth: 1,
+    width: width - 80,
+    borderRadius: 5,
+    marginBottom: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "#FAFAFA",
+    fontSize: 14
+  },
+  touchable: {
+      borderRadius: 5,
+      backgroundColor: "#3E99EE",
+      width: width - 80
+  },
+  button: {
+      paddingHorizontal: 7,
+      height: 50,
+      justifyContent: "center"
+  },
+  btnText: {
+      color: "white",
+      fontWeight: "600",
+      textAlign: "center",
+      fontSize: 14
+  },
+  signupView: {
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "row",
+      height: 50,
+      borderTopWidth: 1,
+      borderTopColor: "#bbb"
+  },
+    signupText: {
+        color: "gray",
+        fontSize: 14,
+        fontWeight: "600"
     },
-    header: { 
-        flex: 1,
-        backgroundColor: "#4E65B4",
-        alignItems: "center",
-        justifyContent: "center",
-        width
-    },
-    logo: {
-        width: 180,
-        height: 65,
-        marginTop: 20
-    },
-    content: {
-        flex: 4,
-        backgroundColor: "white",
-        paddingTop: 20,
-        alignItems: "center",
-        justifyContent: "flex-start",
-    },
-    fbContainer: {
-        marginTop: 50
-    },
-    fbView: {
-        flexDirection: "row",
-        alignItems: "center"
-    },
-    fbText: {
-        color: "#3E99EE",
+    signupLink: {
         marginLeft: 10,
-        fontWeight: "600",
-        fontSize: 14
-    },
+        color: "#3E99EE",
+        fontSize: 14,
+        fontWeight: "600"
+    }
 });
 
 export default LoginScreen;
